d05c8727e2967eb43dfdeb826687539b9255a8f2
keyaki-namiki <28521374+keyaki-namiki@users.noreply.github.com>
2018-12-02 14:28:29 +0900

    some modifications

--- crkbd/hhkvim.c
+++ crkbd/hhkvim.c
@@ -1,11 +1,11 @@
 #include QMK_KEYBOARD_H
 #include "bootloader.h"
 #ifdef PROTOCOL_LUFA
-	#include "lufa.h"
-	#include "split_util.h"
+    #include "lufa.h"
+    #include "split_util.h"
 #endif
 #ifdef SSD1306OLED
-	#include "ssd1306.h"
+    #include "ssd1306.h"
 #endif
 
 extern keymap_config_t keymap_config;
@@ -28,92 +28,92 @@ extern uint8_t is_master;
 #define _ADJUST 16
 
 enum custom_keycodes {
-	QWERTY = SAFE_RANGE,
-	LOWER,
-	RAISE,
-	ADJUST,
-	BACKLIT,
-	RGBRST
+    QWERTY = SAFE_RANGE,
+    LOWER,
+    RAISE,
+    ADJUST,
+    BACKLIT,
+    RGBRST
 };
 
 enum macro_keycodes {
-	KC_SAMPLEMACRO,
+    KC_SAMPLEMACRO,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-	[_QWERTY] = LAYOUT( \
-	//,-----------------------------------------.						,-----------------------------------------.
-	KC_ESC,	KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,						KC_Y,	KC_U,	KC_I,	KC_O,	KC_P,	KC_GRV,\
-	//|------+------+------+------+------+------|						|------+------+------+------+------+------|
-	KC_LCTL,KC_A,	KC_S,	KC_D,	KC_F,	KC_G,						KC_H,	KC_J,	KC_K,	KC_L,	KC_SCLN,KC_QUOT,\
-	//|------+------+------+------+------+------|						|------+------+------+------+------+------|
-	KC_LGUI,KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,						KC_N,	KC_M,	KC_COMM,KC_DOT,	KC_SLSH,KC_RALT,\
-	//|------+------+------+------+------+------+------|  		|------+------+------+------+------+------+------|
-									KC_LSFT,LOWER,KC_SPC,		KC_BSPC,RAISE,	KC_ENT \
-								//`--------------------'		`--------------------'
-	),
+    [_QWERTY] = LAYOUT( \
+    //,-----------------------------------------.                       ,-----------------------------------------.
+    KC_ESC,	KC_Q,	KC_W,	KC_E,	KC_R,	KC_T,                       KC_Y,	KC_U,	KC_I,	KC_O,	KC_P,	KC_GRV,\
+    //|------+------+------+------+------+------|                       |------+------+------+------+------+------|
+    KC_LCTL,KC_A,	KC_S,	KC_D,	KC_F,	KC_G,                       KC_H,	KC_J,	KC_K,	KC_L,	KC_SCLN,KC_QUOT,\
+    //|------+------+------+------+------+------|                       |------+------+------+------+------+------|
+    KC_LGUI,KC_Z,	KC_X,	KC_C,	KC_V,	KC_B,                       KC_N,	KC_M,	KC_COMM,KC_DOT,	KC_SLSH,KC_RALT,\
+    //|------+------+------+------+------+------+------|        |------+------+------+------+------+------+------|
+                                    KC_LSFT,LOWER,KC_SPC,       KC_BSPC,RAISE,  KC_ENT \
+                                //`--------------------'        `--------------------'
+    ),
 
-	[_LOWER] = LAYOUT( \
-	//,-----------------------------------------.						,-----------------------------------------.
-	_______,KC_F1,	KC_F2,	KC_F3,	KC_F4,	KC_F5,						KC_F6,	KC_F7,	KC_F8,	KC_F9,	KC_F10,	_______,\
-	//|------+------+------+------+------+------|						|------+------+------+------+------+------|
-	_______,XXXXXXX,KC_INS,	KC_DEL,	XXXXXXX,KC_F11,						KC_LEFT,KC_DOWN,KC_UP,	KC_RGHT,KC_VOLU,_______,\
-	//|------+------+------+------+------+------|						|------+------+------+------+------+------|
-	_______,XXXXXXX,KC_BSPC,XXXXXXX,KC_ENT,	KC_F12,						KC_HOME,KC_PGDN,KC_PGUP,KC_END,	KC_VOLD,_______,\
-	//|------+------+------+------+------+------+------|		|------+------+------+------+------+------+------|
-									_______,_______,_______,	KC_DEL,	_______,_______ \
-								//`--------------------'		`--------------------'
-	),
+    [_LOWER] = LAYOUT( \
+    //,-----------------------------------------.                       ,-----------------------------------------.
+    _______,KC_F1,	KC_F2,	KC_F3,	KC_F4,	KC_F5,                      KC_F6,	KC_F7,	KC_F8,	KC_F9,	KC_F10,	_______,\
+    //|------+------+------+------+------+------|                       |------+------+------+------+------+------|
+    _______,XXXXXXX,KC_INS,	KC_DEL,	XXXXXXX,KC_F11,                     KC_LEFT,KC_DOWN,KC_UP,	KC_RGHT,KC_VOLU,_______,\
+    //|------+------+------+------+------+------|                       |------+------+------+------+------+------|
+    _______,XXXXXXX,KC_BSPC,XXXXXXX,KC_ENT,	KC_F12,                     KC_HOME,KC_PGDN,KC_PGUP,KC_END,	KC_VOLD,_______,\
+    //|------+------+------+------+------+------+------|       |------+------+------+------+------+------+------|
+                                    _______,_______,_______,    KC_DEL, _______,_______ \
+                                //`--------------------'        `--------------------'
+    ),
 
-	[_RAISE] = LAYOUT( \
-	//,-----------------------------------------.						,-----------------------------------------.
-	_______,KC_1,	KC_2,	KC_3,	KC_4,	KC_5,						KC_6,	KC_7,	KC_8,	KC_9,	KC_0,	KC_DEL,\
-	//|------+------+------+------+------+------|						|------+------+------+------+------+------|
-	_______,KC_4,	KC_5,	KC_6,	KC_DOT,	XXXXXXX,					KC_LBRC,KC_MINS,KC_EQL,	KC_BSLS,KC_LPRN,KC_RPRN,\
-	//|------+------+------+------+------+------|						|------+------+------+------+------+------|
-	_______,KC_7,	KC_8,	KC_9,	KC_0,	XXXXXXX,					KC_RBRC,KC_SLSH,KC_LT,	KC_GT,	XXXXXXX,_______,\
-	//|------+------+------+------+------+------+------|		|------+------+------+------+------+------+------|
-									_______,_______,KC_TAB,		_______,_______,_______
-								//`--------------------'		`--------------------'
-	),
+    [_RAISE] = LAYOUT( \
+    //,-----------------------------------------.                       ,-----------------------------------------.
+    _______,KC_1,	KC_2,	KC_3,	KC_4,	KC_5,                       KC_6,	KC_7,	KC_8,	KC_9,	KC_0,	KC_DEL,\
+    //|------+------+------+------+------+------|                       |------+------+------+------+------+------|
+    _______,KC_4,	KC_5,	KC_6,	KC_DOT,	XXXXXXX,                    KC_LBRC,KC_MINS,KC_EQL,	KC_BSLS,KC_LPRN,KC_RPRN,\
+    //|------+------+------+------+------+------|                       |------+------+------+------+------+------|
+    _______,KC_7,	KC_8,	KC_9,	KC_0,	XXXXXXX,                    KC_RBRC,KC_SLSH,KC_LT,	KC_GT,	XXXXXXX,_______,\
+    //|------+------+------+------+------+------+------|        |------+------+------+------+------+------+------|
+                                    _______,_______,KC_TAB,     _______,_______,_______
+                                //`--------------------'        `--------------------'
+    ),
 
-	[_ADJUST] = LAYOUT( \
-	//,-----------------------------------------.						,-----------------------------------------.
-	_______,RESET,	AG_NORM,XXXXXXX,XXXXXXX,QWERTY,						XXXXXXX,XXXXXXX,KC_PSCR, XXXXXXX,XXXXXXX,XXXXXXX,\
-	//|------+------+------+------+------+------|						|------+------+------+------+------+------|
-	_______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,					RGBRST,	RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,XXXXXXX,\
-	//|------+------+------+------+------+------|						|------+------+------+------+------+------|
-	_______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,					XXXXXXX,RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,_______,\
-	//|------+------+------+------+------+------+------|		|------+------+------+------+------+------+------|
-									_______,_______,XXXXXXX,	XXXXXXX,_______,XXXXXXX \
-								//`--------------------'		`--------------------'
-	)
+    [_ADJUST] = LAYOUT( \
+    //,-----------------------------------------.                       ,-----------------------------------------.
+    _______,RESET,  AG_NORM,XXXXXXX,XXXXXXX,QWERTY,                     XXXXXXX,XXXXXXX,KC_PSCR, XXXXXXX,XXXXXXX,XXXXXXX,\
+    //|------+------+------+------+------+------|                       |------+------+------+------+------+------|
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                    RGBRST,	RGB_TOG,RGB_HUI,RGB_SAI,RGB_VAI,XXXXXXX,\
+    //|------+------+------+------+------+------|                       |------+------+------+------+------+------|
+    _______,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,                    XXXXXXX,RGB_MOD,RGB_HUD,RGB_SAD,RGB_VAD,_______,\
+    //|------+------+------+------+------+------+------|        |------+------+------+------+------+------+------|
+                                    _______,_______,XXXXXXX,    XXXXXXX,_______,XXXXXXX \
+                                //`--------------------'        `--------------------'
+    )
 };
 
 int RGB_current_mode;
 
 void persistent_default_layer_set(uint16_t default_layer) {
-	eeconfig_update_default_layer(default_layer);
-	default_layer_set(default_layer);
+    eeconfig_update_default_layer(default_layer);
+    default_layer_set(default_layer);
 }
 
 // Setting ADJUST layer RGB back to default
 void update_tri_layer_RGB(uint8_t layer1, uint8_t layer2, uint8_t layer3) {
-	if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
-		layer_on(layer3);
-	} else {
-		layer_off(layer3);
-	}
+    if (IS_LAYER_ON(layer1) && IS_LAYER_ON(layer2)) {
+        layer_on(layer3);
+    } else {
+        layer_off(layer3);
+    }
 }
 
 void matrix_init_user(void) {
-		#ifdef RGBLIGHT_ENABLE
-			RGB_current_mode = rgblight_config.mode;
-		#endif
-		//SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
-		#ifdef SSD1306OLED
-				iota_gfx_init(!has_usb());   // turns on the display
-		#endif
+        #ifdef RGBLIGHT_ENABLE
+            RGB_current_mode = rgblight_config.mode;
+        #endif
+        //SSD1306 OLED init, make sure to add #define SSD1306OLED in config.h
+        #ifdef SSD1306OLED
+                iota_gfx_init(!has_usb());   // turns on the display
+        #endif
 }
 
 //SSD1306 OLED update loop, make sure to add #define SSD1306OLED in config.h
@@ -132,99 +132,99 @@ const char *read_keylogs(void);
 // const char *read_timelog(void);
 
 void matrix_scan_user(void) {
-	iota_gfx_task();
+    iota_gfx_task();
 }
 
 void matrix_render_user(struct CharacterMatrix *matrix) {
-	if (is_master) {
-		// If you want to change the display of OLED, you need to change here
-		matrix_write_ln(matrix, read_layer_state());
-		matrix_write_ln(matrix, read_keylog());
-		matrix_write_ln(matrix, read_keylogs());
-		//matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
-		//matrix_write_ln(matrix, read_host_led_state());
-		//matrix_write_ln(matrix, read_timelog());
-	} else {
-		matrix_write(matrix, read_logo());
-	}
+    if (is_master) {
+        // If you want to change the display of OLED, you need to change here
+        matrix_write_ln(matrix, read_layer_state());
+        matrix_write_ln(matrix, read_keylog());
+        matrix_write_ln(matrix, read_keylogs());
+        //matrix_write_ln(matrix, read_mode_icon(keymap_config.swap_lalt_lgui));
+        //matrix_write_ln(matrix, read_host_led_state());
+        //matrix_write_ln(matrix, read_timelog());
+    } else {
+        matrix_write(matrix, read_logo());
+    }
 }
 
 void matrix_update(struct CharacterMatrix *dest, const struct CharacterMatrix *source) {
-	if (memcmp(dest->display, source->display, sizeof(dest->display))) {
-		memcpy(dest->display, source->display, sizeof(dest->display));
-		dest->dirty = true;
-	}
+    if (memcmp(dest->display, source->display, sizeof(dest->display))) {
+        memcpy(dest->display, source->display, sizeof(dest->display));
+        dest->dirty = true;
+    }
 }
 
 void iota_gfx_task_user(void) {
-	struct CharacterMatrix matrix;
-	matrix_clear(&matrix);
-	matrix_render_user(&matrix);
-	matrix_update(&display, &matrix);
+    struct CharacterMatrix matrix;
+    matrix_clear(&matrix);
+    matrix_render_user(&matrix);
+    matrix_update(&display, &matrix);
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-	if (record->event.pressed) {
-		set_keylog(keycode, record);
-		// set_timelog();
-	}
+    if (record->event.pressed) {
+        set_keylog(keycode, record);
+        // set_timelog();
+    }
 
-	switch (keycode) {
-		case QWERTY:
-			if (record->event.pressed) {
-				persistent_default_layer_set(1UL<<_QWERTY);
-			}
-			return false;
-			break;
-		case LOWER:
-			if (record->event.pressed) {
-				layer_on(_LOWER);
-				update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-			} else {
-				layer_off(_LOWER);
-				update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-			}
-			return false;
-			break;
-		case RAISE:
-			if (record->event.pressed) {
-				layer_on(_RAISE);
-				update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-			} else {
-				layer_off(_RAISE);
-				update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
-			}
-			return false;
-			break;
-		case ADJUST:
-				if (record->event.pressed) {
-					layer_on(_ADJUST);
-				} else {
-					layer_off(_ADJUST);
-				}
-				return false;
-				break;
-		case RGB_MOD:
-			#ifdef RGBLIGHT_ENABLE
-				if (record->event.pressed) {
-					rgblight_mode(RGB_current_mode);
-					rgblight_step();
-					RGB_current_mode = rgblight_config.mode;
-				}
-			#endif
-			return false;
-			break;
-		case RGBRST:
-			#ifdef RGBLIGHT_ENABLE
-				if (record->event.pressed) {
-					eeconfig_update_rgblight_default();
-					rgblight_enable();
-					RGB_current_mode = rgblight_config.mode;
-				}
-			#endif
-			break;
-	}
-	return true;
+    switch (keycode) {
+        case QWERTY:
+            if (record->event.pressed) {
+                persistent_default_layer_set(1UL<<_QWERTY);
+            }
+            return false;
+            break;
+        case LOWER:
+            if (record->event.pressed) {
+                layer_on(_LOWER);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_LOWER);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+        case RAISE:
+            if (record->event.pressed) {
+                layer_on(_RAISE);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            } else {
+                layer_off(_RAISE);
+                update_tri_layer_RGB(_LOWER, _RAISE, _ADJUST);
+            }
+            return false;
+            break;
+        case ADJUST:
+                if (record->event.pressed) {
+                    layer_on(_ADJUST);
+                } else {
+                    layer_off(_ADJUST);
+                }
+                return false;
+                break;
+        case RGB_MOD:
+            #ifdef RGBLIGHT_ENABLE
+                if (record->event.pressed) {
+                    rgblight_mode(RGB_current_mode);
+                    rgblight_step();
+                    RGB_current_mode = rgblight_config.mode;
+                }
+            #endif
+            return false;
+            break;
+        case RGBRST:
+            #ifdef RGBLIGHT_ENABLE
+                if (record->event.pressed) {
+                    eeconfig_update_rgblight_default();
+                    rgblight_enable();
+                    RGB_current_mode = rgblight_config.mode;
+                }
+            #endif
+            break;
+    }
+    return true;
 }
 
 #endif
